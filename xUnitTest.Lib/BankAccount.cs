9ddf1c7b25c474b2abb1059bb49cbc4e4e1e2e51
Aumnuay Niranartkul <>
2018-03-18 17:02:50 +0700

    .gitignore is now working

---
+++ xUnitTest.Lib/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace xUnitTest.Lib
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; } = 0;
+        public BankAccount(int startingBalance)
+        {
+            this.Balance = startingBalance;    
+        }
+        public void Deposit(int amount){
+            if(amount<0)
+                throw new ArgumentException("Deposit amount must be positive.",nameof(amount));
+            this.Balance += amount;
+        }
+        public void Withdraw(int amount){
+            this.Balance -= amount;
+        }
+
+    }
+}
