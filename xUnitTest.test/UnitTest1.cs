7e5fbce142f44955c038f77a6268f412de57398b
Aumnuay Niranartkul <aumnuayniranartkul@Aumnuays-MacBook-Pro.local>
2018-03-18 17:07:48 +0700

    test throws

--- xUnitTest.test/UnitTest1.cs
+++ xUnitTest.test/UnitTest1.cs
@@ -1,14 +1,48 @@
 using System;
 using Xunit;
+using xUnitTest.Lib;
 
 namespace xUnitTest.test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        const int init = 100;
+         BankAccount ba = new BankAccount(init);
+        public  UnitTest1()
         {
 
+        }
+        [Theory]
+        [InlineData(100)]
+        [InlineData(200)]
+        [InlineData(300)]
+        public void BankAccountShouldIncreaseOnPositionDeposit(int amount){
+            
+            ba.Deposit(amount);
+            Assert.True(ba.Balance>init);
+            Assert.True(ba.Balance>0);
+        }
+        [Theory]
+        [InlineData(-100)]
+        public void BankAccountShouldThrowOnNonPositiveAmount(int amount){
+            Action act = () => ba.Deposit(amount);
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+            Assert.Equal(1,1);
+            
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
         }
     }
 }
